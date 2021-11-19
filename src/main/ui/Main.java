d69933ec9dc10a36248c99c445f8d6e204ef09b6
achan534 <achan534@student.ubc.ca>
2021-11-19 03:01:21 -0800

    Updated the Main class s.t. it runs the GUI.

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -3,9 +3,7 @@ package ui;
 // Main class used to run the MyMovieList application
 public class Main {
     public static void main(String[] args) {
-        // Creates a new MyMovieListApp object
-        MyMovieListApp myMovieListApp = new MyMovieListApp();
         // Runs the app
-        myMovieListApp.runMainMenu();
+        new MyMovieListUI();
     }
 }
